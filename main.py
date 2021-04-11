5e6f77ac5a28a1bca680a0e18b1d23fe3070b860
xmtaha <82343366+xmtaha@users.noreply.github.com>
2021-04-12 00:58:28 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import tweepy
+import time
+
+auth = tweepy.OAuthHandler('--api key buraya--', '--secret api key buraya--')
+auth.set_access_token('--access token buraya--', '--secret access token buraya--')
+
+api = tweepy.API(auth)
+user = api.me()
+
+# Beğeni arası bekleme süresi.. Şuan: 10sn
+def limit_handle(cursor):
+    try:
+        while True:
+            yield cursor.next()
+    except tweepy.RateLimitError:
+        time.sleep(10000)
+
+
+# Kaç tweet beğenilecek. Şuan: 50
+for tweet in tweepy.Cursor(api.home_timeline).items(50):
+    try:
+        limit_handle(tweet.favorite())
+    except tweepy.TweepError as e:
+        error = True
+        print(e.reason)
